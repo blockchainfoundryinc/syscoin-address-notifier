9d790d4613e0fe1d2ccecd193293db5fbbda4f6f
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2020-02-10 14:38:08 -0500

    ssl

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ dotenv.config({ silent: true });
 
 module.exports = {
   zmq_address: process.env.ZMQ_ADDRESS || 'tcp://127.0.0.1:28332',
-  ws_port: process.env.WA_PORT || 9999,
+  ws_port: process.env.WS_PORT || 9999,
   zdag_check_time: process.env.ZDAG_CHECK_TIME || 10,
   rpc: {
     host: process.env.RPC_HOST || "localhost",
@@ -12,7 +12,7 @@ module.exports = {
     password: process.env.RPC_PASS || "p",
     logLevel: process.env.LOG_LEVEL || 'error'
   },
-  use_ssl: false,
-  ssl_key: '',
-  ssl_cert: ''
+  use_ssl: process.env.USE_SSL || false,
+  ssl_key: process.env.SSL_KEY || '',
+  ssl_cert: process.env.SSL_CERT  || ''
 };
