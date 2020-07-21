83e7984030c02e2b62d99741570962a2677e7943
dwasyluk <987186+dwasyluk@users.noreply.github.com>
2020-07-21 16:12:06 -0400

    fix

--- message-handlers.js
+++ message-handlers.js
@@ -43,7 +43,7 @@ async function handleRawTxMessage(topic, message, txData, io, isZdag, zdagMessag
 
   // see if we already have an entry for this tx
   const entryExists = txData.unconfirmedTxToAddressArr.find(entry => entry.txid === tx.txid);
-  const blockHeight = (await rpc.getBlockchainInfo().call())
+  let blockHeight = (await rpc.getBlockchainInfo().call())
   console.log("Blocks:", blockHeight);
   blockHeight = blockHeight.blocks;
   if (!entryExists) {
